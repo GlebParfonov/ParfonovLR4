b6d3f38e770d2b872204ce757a7038e4a5fa90b7
GlebParfonov <glbprfnv@ukr.net>
2023-01-08 21:15:24 +0200

    LR4

---
+++ Parfonov_LR4_RE-21mp/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace Parfonov_LR4_RE_21mp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label_time = new System.Windows.Forms.Label();
+            this.button_fileIN = new System.Windows.Forms.Button();
+            this.button_fileOUT = new System.Windows.Forms.Button();
+            this.button_KEYgenerator = new System.Windows.Forms.Button();
+            this.button_start = new System.Windows.Forms.Button();
+            this.button_fileKEYsave = new System.Windows.Forms.Button();
+            this.button_fileKEYopen = new System.Windows.Forms.Button();
+            this.textBox_fileIN = new System.Windows.Forms.TextBox();
+            this.textBox_fileOUT = new System.Windows.Forms.TextBox();
+            this.textBox_fileKEY = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label_roz = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Відкрити файл";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 58);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Зберегти у файлі";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 36);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Файо з ключем";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 290);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(201, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = " Тривалість шифрування (мін: сек. мс)";
+            // 
+            // label_time
+            // 
+            this.label_time.AutoSize = true;
+            this.label_time.Location = new System.Drawing.Point(246, 290);
+            this.label_time.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label_time.Name = "label_time";
+            this.label_time.Size = new System.Drawing.Size(49, 13);
+            this.label_time.TabIndex = 4;
+            this.label_time.Text = "00:00:00";
+            // 
+            // button_fileIN
+            // 
+            this.button_fileIN.Location = new System.Drawing.Point(308, 24);
+            this.button_fileIN.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button_fileIN.Name = "button_fileIN";
+            this.button_fileIN.Size = new System.Drawing.Size(56, 19);
+            this.button_fileIN.TabIndex = 5;
+            this.button_fileIN.Text = "...";
+            this.button_fileIN.UseVisualStyleBackColor = true;
+            this.button_fileIN.Click += new System.EventHandler(this.button_fileIN_Click);
+            // 
+            // button_fileOUT
+            // 
+            this.button_fileOUT.Location = new System.Drawing.Point(308, 58);
+            this.button_fileOUT.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button_fileOUT.Name = "button_fileOUT";
+            this.button_fileOUT.Size = new System.Drawing.Size(56, 19);
+            this.button_fileOUT.TabIndex = 6;
+            this.button_fileOUT.Text = "...";
+            this.button_fileOUT.UseVisualStyleBackColor = true;
+            this.button_fileOUT.Click += new System.EventHandler(this.button_fileOUT_Click);
+            // 
+            // button_KEYgenerator
+            // 
+            this.button_KEYgenerator.Image = global::Parfonov_LR4_RE_21mp.Properties.Resources.клю;
+            this.button_KEYgenerator.Location = new System.Drawing.Point(166, 63);
+            this.button_KEYgenerator.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button_KEYgenerator.Name = "button_KEYgenerator";
+            this.button_KEYgenerator.Size = new System.Drawing.Size(63, 36);
+            this.button_KEYgenerator.TabIndex = 7;
+            this.button_KEYgenerator.UseVisualStyleBackColor = true;
+            this.button_KEYgenerator.Click += new System.EventHandler(this.button_KEYgenerator_Click);
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(328, 288);
+            this.button_start.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(56, 19);
+            this.button_start.TabIndex = 10;
+            this.button_start.Text = "Старт";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // button_fileKEYsave
+            // 
+            this.button_fileKEYsave.Image = global::Parfonov_LR4_RE_21mp.Properties.Resources.СОХРАНИТЬ;
+            this.button_fileKEYsave.Location = new System.Drawing.Point(318, 54);
+            this.button_fileKEYsave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button_fileKEYsave.Name = "button_fileKEYsave";
+            this.button_fileKEYsave.Size = new System.Drawing.Size(46, 50);
+            this.button_fileKEYsave.TabIndex = 9;
+            this.toolTip2.SetToolTip(this.button_fileKEYsave, "Зберегти");
+            this.button_fileKEYsave.UseVisualStyleBackColor = true;
+            this.button_fileKEYsave.Click += new System.EventHandler(this.button_fileKEYsave_Click);
+            // 
+            // button_fileKEYopen
+            // 
+            this.button_fileKEYopen.Image = global::Parfonov_LR4_RE_21mp.Properties.Resources.ОТКРЫТЬ;
+            this.button_fileKEYopen.Location = new System.Drawing.Point(268, 54);
+            this.button_fileKEYopen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button_fileKEYopen.Name = "button_fileKEYopen";
+            this.button_fileKEYopen.Size = new System.Drawing.Size(46, 50);
+            this.button_fileKEYopen.TabIndex = 8;
+            this.toolTip1.SetToolTip(this.button_fileKEYopen, "Відкрити");
+            this.button_fileKEYopen.UseVisualStyleBackColor = true;
+            this.button_fileKEYopen.Click += new System.EventHandler(this.button_fileKEYopen_Click);
+            // 
+            // textBox_fileIN
+            // 
+            this.textBox_fileIN.Location = new System.Drawing.Point(142, 77);
+            this.textBox_fileIN.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox_fileIN.Name = "textBox_fileIN";
+            this.textBox_fileIN.Size = new System.Drawing.Size(163, 20);
+            this.textBox_fileIN.TabIndex = 11;
+            // 
+            // textBox_fileOUT
+            // 
+            this.textBox_fileOUT.Location = new System.Drawing.Point(155, 108);
+            this.textBox_fileOUT.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox_fileOUT.Name = "textBox_fileOUT";
+            this.textBox_fileOUT.Size = new System.Drawing.Size(150, 20);
+            this.textBox_fileOUT.TabIndex = 12;
+            // 
+            // textBox_fileKEY
+            // 
+            this.textBox_fileKEY.Location = new System.Drawing.Point(110, 31);
+            this.textBox_fileKEY.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox_fileKEY.Name = "textBox_fileKEY";
+            this.textBox_fileKEY.Size = new System.Drawing.Size(183, 20);
+            this.textBox_fileKEY.TabIndex = 13;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button_fileIN);
+            this.groupBox1.Controls.Add(this.button_fileOUT);
+            this.groupBox1.Location = new System.Drawing.Point(32, 52);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(368, 98);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Файли";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label_roz);
+            this.groupBox2.Controls.Add(this.textBox_fileKEY);
+            this.groupBox2.Controls.Add(this.button_KEYgenerator);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.button_fileKEYsave);
+            this.groupBox2.Controls.Add(this.button_fileKEYopen);
+            this.groupBox2.Location = new System.Drawing.Point(32, 155);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Size = new System.Drawing.Size(368, 115);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ключ";
+            // 
+            // label_roz
+            // 
+            this.label_roz.AutoSize = true;
+            this.label_roz.Location = new System.Drawing.Point(26, 75);
+            this.label_roz.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label_roz.Name = "label_roz";
+            this.label_roz.Size = new System.Drawing.Size(79, 13);
+            this.label_roz.TabIndex = 15;
+            this.label_roz.Text = "Розмір коюча:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 323);
+            this.Controls.Add(this.textBox_fileOUT);
+            this.Controls.Add(this.textBox_fileIN);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.label_time);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label_time;
+        private System.Windows.Forms.Button button_fileIN;
+        private System.Windows.Forms.Button button_fileOUT;
+        private System.Windows.Forms.Button button_KEYgenerator;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Button button_fileKEYsave;
+        private System.Windows.Forms.Button button_fileKEYopen;
+        private System.Windows.Forms.TextBox textBox_fileIN;
+        private System.Windows.Forms.TextBox textBox_fileOUT;
+        private System.Windows.Forms.TextBox textBox_fileKEY;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.Label label_roz;
+    }
+}
+
